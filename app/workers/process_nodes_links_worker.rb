89694e68f1aca9967dce99c2b9ffd89cef945971
Addison Martin <addisonmartin@hey.com>
2021-07-21 02:04:16 -0400

    Remove dead code

--- app/workers/process_nodes_links_worker.rb
+++ app/workers/process_nodes_links_worker.rb
@@ -15,38 +15,29 @@ class ProcessNodesLinksWorker
       node.update_column(:began_processing_links, true)
       node.update_column(:error_processing_links, false)
 
-      #begin
-        continue_query = nil
-        while true
-          query_options = {pllimit: 500}
-          query_options.merge!({plcontinue: continue_query}) if continue_query
+      continue_query = nil
+      while true
+        query_options = {pllimit: 500}
+        query_options.merge!({plcontinue: continue_query}) if continue_query
 
-          wikipedia_content = Wikipedia.find(node.name, query_options)
-          unless wikipedia_content.links.nil?
-            wikipedia_content.links.each do |link_name|
-              linked_node = Node.find_or_create_by_name(link_name, node.graph)
-              Edge.find_or_create_from_nodes(node, linked_node, node.graph)
-            end
+        wikipedia_content = Wikipedia.find(node.name, query_options)
+        unless wikipedia_content.links.nil?
+          wikipedia_content.links.each do |link_name|
+            linked_node = Node.find_or_create_by_name(link_name, node.graph)
+            Edge.find_or_create_from_nodes(node, linked_node, node.graph)
           end
-
-          continue_query = wikipedia_content.raw_data.dig('continue', 'plcontinue')
-          break if continue_query.nil?
         end
 
-        node.update_column(:ended_processing_links, true)
-        node.update_column(:error_processing_links, false)
-        node.save!
+        continue_query = wikipedia_content.raw_data.dig('continue', 'plcontinue')
+        break if continue_query.nil?
+      end
 
-        puts "#{Time.zone.now}"
-        puts "Processed links for: #{node.name}"
-      #rescue => error
-      # node.update_column(:error_processing_links, true)
-      #  node.update_column(:ended_processing_links, false)
+      node.update_column(:ended_processing_links, true)
+      node.update_column(:error_processing_links, false)
+      node.save!
 
-      #puts "#{Time.zone.now}"
-      # puts "ERROR: Processing #{node.name}'s links: #{error}"
-      #  puts "#{error.backtrace.join("\n")}"
-      #end
+      puts "#{Time.zone.now}"
+      puts "Processed links for: #{node.name}"
     end
   end
 end
